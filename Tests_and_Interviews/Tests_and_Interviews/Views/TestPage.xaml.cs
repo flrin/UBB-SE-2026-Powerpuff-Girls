844d84475233fad14f4b03b3b2e071787f85e6eb
florin <113235221+flrin@users.noreply.github.com>
2026-03-16 23:51:02 +0200

    Added database service

--- Tests_and_Interviews/Tests_and_Interviews/Views/TestPage.xaml.cs
+++ Tests_and_Interviews/Tests_and_Interviews/Views/TestPage.xaml.cs
@@ -20,9 +20,7 @@ using Tests_and_Interviews.ViewModels;
 
 namespace Tests_and_Interviews.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class TestPage : Page
     {
         public TestViewModel ViewModel { get; }
