a27cd93d63d68b1fe99348e640caaf7cd6590120
LarisaD52 <larisa.dragea@stud.ubbcluj.ro>
2026-03-21 18:21:31 +0200

    candidateHomeView

---
+++ Tests_and_Interviews/Tests_and_Interviews/Services/BookingService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tests_and_Interviews.Domain;
+using Tests_and_Interviews.Repositories;
+
+namespace Tests_and_Interviews.Services
+{
+    public class BookingService
+    {
+        private readonly SlotRepository _slotRepo;
+        public BookingService()
+        {
+            _slotRepo = new SlotRepository();
+        }
+
+        public List<Slot> getAvailableSlots(int recruiterId, DateTime date)
+        {
+            return _slotRepo
+                .GetSlots(recruiterId, date)
+                .Where(s => s.IsAvailable())
+                .ToList();
+        }
+
+        public void confirmBooking(int candidateId, int slotId)
+        {
+            var slot = _slotRepo.GetById(slotId);
+            if (slot == null)
+                throw new Exception("Slot not found");
+
+            if (!slot.IsAvailable())
+                throw new Exception("This slot is no longer available");
+            slot.Lock(candidateId);
+            _slotRepo.Update(slot);
+        }
+    }
+}
---
+++ Tests_and_Interviews/Tests_and_Interviews/ViewModels/CandidateViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Tests_and_Interviews.Helpers;
+using Tests_and_Interviews.Services;
+using Tests_and_Interviews.Domain;
+
+namespace Tests_and_Interviews.ViewModels
+{
+    public class CandidateViewModel : INotifyPropertyChanged
+    {
+        private readonly BookingService _bookingService;
+        private List<Slot> _availableSlots;
+        public ICommand LoadSlotsCommand { get; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public CandidateViewModel()
+        {
+            _bookingService = new BookingService();
+            LoadSlotsCommand = new RelayCommand(LoadSlots);
+
+            LoadSlots();
+        }
+
+        public List<Slot> AvailableSlots
+        {
+            get => _availableSlots;
+            set
+            {
+                if (_availableSlots != value)
+                {
+                    _availableSlots = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void LoadSlots()
+        {
+            AvailableSlots = _bookingService.getAvailableSlots(1, DateTime.Today);
+        }
+    }
+}
