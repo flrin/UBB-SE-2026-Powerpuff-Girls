6826fd4256947591cec8a06146152e01196bf1f3
LarisaD52 <larisa.dragea@stud.ubbcluj.ro>
2026-03-21 20:48:14 +0200

    candidateHomeView

--- Tests_and_Interviews/Tests_and_Interviews/Models/Slot.cs
+++ Tests_and_Interviews/Tests_and_Interviews/Models/Slot.cs
@@ -5,25 +5,13 @@ namespace Tests_and_Interviews.Domain
     public class Slot
     {
         public int Id { get; set; }
-
         public int RecruiterId { get; set; }
-
         public int CandidateId { get; set; }
-
         public DateTime StartTime { get; set; }
-
         public DateTime EndTime { get; set; }
-
         public int Duration { get; set; }
-
         public SlotStatus Status { get; set; }
-
-        public string InterviewType { get; set; }
-
-        // -----------------------
-        // METHODS (din diagramă)
-        // -----------------------
-
+        
         public bool IsAvailable()
         {
             return Status == SlotStatus.Free;
@@ -38,6 +26,7 @@ namespace Tests_and_Interviews.Domain
             CandidateId = candidateId;
         }
 
+        
         public void Release()
         {
             Status = SlotStatus.Free;
